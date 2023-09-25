5dea859c2e0ad124cea1c5384039e90c7aea5a18
Mahir <mahr.bwb@gmail.com>
2023-09-25 17:16:31 +0300

    refactor init state and Navigate to home screen

--- lib/Features/splash/presentation/widgets/splash_screen_body.dart
+++ lib/Features/splash/presentation/widgets/splash_screen_body.dart
@@ -1,6 +1,10 @@
 import 'package:bookly/Features/splash/presentation/widgets/sliding_text.dart';
 import 'package:bookly/core/utils/assets/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../constants.dart';
+import '../../../Home/presentation/screens/home_screen.dart';
 
 class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
@@ -16,17 +20,11 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
 
   @override
   void initState() {
-    _animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-
-    _slideAnimation =
-        Tween<Offset>(begin: const Offset(0, 3), end: const Offset(0, 0))
-            .animate(_animationController);
-
-    _animationController.forward();
-
     super.initState();
+    initSlidingAnimation();
+    navigateToHome();
   }
+
 
   @override
   void dispose() {
@@ -47,5 +45,23 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
         SlidingText(slideAnimation: _slideAnimation),
       ],
     );
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(const HomeScreen(),
+          transition: Transition.circularReveal, duration: kTransitionDuration);
+    });
+  }
+
+  void initSlidingAnimation() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    _slideAnimation =
+        Tween<Offset>(begin: const Offset(0, 3), end: const Offset(0, 0))
+            .animate(_animationController);
+
+    _animationController.forward();
   }
 }
