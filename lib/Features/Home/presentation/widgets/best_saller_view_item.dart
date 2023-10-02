97069495c58ca6f46ae93dc82be511006ba23081
Mahir <mahr.bwb@gmail.com>
2023-10-02 11:45:18 +0300

    bestSeller part three

---
+++ lib/Features/Home/presentation/widgets/best_saller_view_item.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../../../../constants.dart';
+import '../../../../core/utils/assets/assets.dart';
+import '../../../../core/utils/styles.dart';
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.25,
+              width: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.red,
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(
+                  'Harry Potter and The Global of Fire ',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                ),
+              ),
+              const SizedBox(height: 3),
+              const Text('J.K.Rowling', style: Styles.textStyle14),
+              Row(
+                children: [
+                  Text('19.9 \$',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold)),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/Features/Home/presentation/widgets/home_body.dart
+++ lib/Features/Home/presentation/widgets/home_body.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'best_saller_view_item.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view_items.dart';
 
@@ -29,48 +31,3 @@ class HomeScreenBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.25,
-              width: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.red,
-                image: const DecorationImage(
-                  image: AssetImage(AssetsData.testImage),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: const Text(
-                  'Harry Potter and The Global of Fire ',
-                  maxLines: 2,
-
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
-                ),
-              ),
-
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
