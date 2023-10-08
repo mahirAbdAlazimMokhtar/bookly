0a6da775a08655a6709e54335eb0e04de84e7082
Mahir <mahr.bwb@gmail.com>
2023-10-08 11:30:49 +0300

    create search view and  route

---
+++ lib/Features/Search/presentation/screens/search_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/search_view_body.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SearchViewBody()
+    );
+  }
+}
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,11 +2,13 @@ import 'package:bookly/Features/Home/presentation/screens/book_details_View.dart
 import 'package:bookly/Features/Home/presentation/screens/home_screen.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../Features/Search/presentation/screens/search_screen.dart';
 import '../../Features/splash/presentation/screens/splash_view.dart';
 
 abstract class AppRouter {
   static const splashScreen = '/';
   static const homeScreen = '/homeScreen';
+  static const searchScreen = '/searchScreen';
   static const bookDetailsScreen = '/detailsScreen';
 
   static final router = GoRouter(routes: [
@@ -21,6 +23,10 @@ abstract class AppRouter {
     GoRoute(
       path: bookDetailsScreen,
       builder: (context, state) => const BookDetailView(),
+    ),
+    GoRoute(
+      path: searchScreen,
+      builder: (context, state) => const SearchScreen(),
     ),
   ]);
 }
