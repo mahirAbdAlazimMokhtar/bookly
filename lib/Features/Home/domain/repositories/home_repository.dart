4ab83b49beb45a0430dde4b90a29fc86ecc1bbbb
Mahir <mahr.bwb@gmail.com>
2023-10-15 10:07:14 +0300

    add Either class and Failure ✅

--- lib/Features/Home/domain/repositories/home_repository.dart
+++ lib/Features/Home/domain/repositories/home_repository.dart
@@ -1,6 +1,9 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
 import '../entities/book_entity.dart';
 
 abstract class HomeRepository {
-  Future<List<BookEntity>> getBooks();
-  Future<List<BookEntity>> getNewsBook();
+  Future<Either<Failure,List<BookEntity>>> getBooks();
+  Future<Either<Failure,List<BookEntity>>> getNewsBook();
 }
