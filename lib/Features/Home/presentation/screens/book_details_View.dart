15a912e0f22162783dd9fa04a694cc4e6f460522
Mahir <mahr.bwb@gmail.com>
2023-10-19 08:28:01 +0300

    add book_model to data layer✅

--- lib/Features/Home/presentation/screens/book_details_View.dart
+++ lib/Features/Home/presentation/screens/book_details_View.dart
@@ -1,5 +1,7 @@
-import 'package:bookly/Features/Home/presentation/widgets/book_detail_widgets/book_detail_body.dart';
+
 import 'package:flutter/material.dart';
+
+import '../widgets/book_detail_widgets/book_detail_body.dart';
 
 class BookDetailView extends StatelessWidget {
   const BookDetailView({super.key});
